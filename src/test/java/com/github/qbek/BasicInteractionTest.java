35bb37acd5fa75254dfd8123e1eb7a3f3cdbc365
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2019-12-11 14:38:57 +0100

    Handling checkboxex

--- src/test/java/com/github/qbek/BasicInteractionTest.java
+++ src/test/java/com/github/qbek/BasicInteractionTest.java
@@ -1,5 +1,6 @@
 package com.github.qbek;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -31,6 +32,31 @@ public class BasicInteractionTest {
 
         assertEquals("Descrpition", "Jakub",  inputText);
         loginButton.click();
+        Thread.sleep(2000);
+        browser.quit();
+    }
+
+    @Test
+    public void checkBoxesTest() throws InterruptedException {
+        WebDriver browser = new FirefoxDriver();
+        browser.get("http://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+        browser.manage().window().maximize();
+
+        WebElement option1Checkbox = browser.findElement(By.cssSelector("[value=\"option-1\"]"));
+        WebElement option2Checkbox = browser.findElement(By.cssSelector("[value=\"option-2\"]"));
+        WebElement option3Checkbox = browser.findElement(By.cssSelector("[value=\"option-3\"]"));
+        WebElement option4Checkbox = browser.findElement(By.cssSelector("[value=\"option-4\"]"));
+
+        boolean isOption1Select = option1Checkbox.isSelected();
+        Assert.assertEquals("Option-1 should be unselected", false, isOption1Select);
+
+        boolean isOption3Selected = option3Checkbox.isSelected();
+        Assert.assertEquals("Option-3 should be selected", true, isOption3Selected);
+
+
+        option2Checkbox.click();
+        option3Checkbox.click();
+
         Thread.sleep(2000);
         browser.quit();
     }
