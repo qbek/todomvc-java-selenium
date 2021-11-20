c639a741bb16ec7a99047bbe49ae01c3f2fb76a1
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2021-11-20 14:37:48 +0100

    Complete todo test

--- src/test/java/org/example/CreateTodoTest.java
+++ src/test/java/org/example/CreateTodoTest.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import net.serenitybdd.core.Serenity;
+import net.thucydides.core.annotations.Managed;
+import org.apache.xerces.impl.xpath.regex.Match;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.After;
@@ -9,6 +12,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 
 public class CreateTodoTest {
@@ -26,6 +34,17 @@ public class CreateTodoTest {
         userOpensTodoMvcApplication();
         userCreatesANewTodo(todoName);
         userChecksIfTodoWasCreated(todoName);
+    }
+
+    @Test
+    public void user_can_complete_the_todo() throws InterruptedException {
+        String todoName = "Zadanie do zakończenia";
+        userOpensTodoMvcApplication();
+        userCreatesANewTodo(todoName);
+        userCompletesTheTodo();
+        userChecksIfTodoIsMarkedAsCompleted();
+        userChecksIfCompletedTodoIsNOTonActiveList(todoName);
+        userChecksIfCompletedTodoIsOnCompletedList(todoName);
     }
 
     private void userOpensTodoMvcApplication() throws InterruptedException {
@@ -46,6 +65,36 @@ public class CreateTodoTest {
         By todoList = By.cssSelector(".todo-list li");
         WebElement todos = browser.findElement(todoList);
         String todoName = todos.getText();
-        MatcherAssert.assertThat("Todo list shoudl contain created todo", todoName, Matchers.equalTo(expectedName));
+        assertThat("Todo list shoudl contain created todo", todoName, equalTo(expectedName));
+    }
+
+    private void userCompletesTheTodo() {
+        By completeToggle = By.cssSelector(".todo-list li .toggle");
+        WebElement toComplete = browser.findElement(completeToggle);
+        toComplete.click();
+    }
+
+    private void userChecksIfTodoIsMarkedAsCompleted() {
+        String completitionMark = "completed";
+        By todoElement = By.cssSelector(".todo-list li");
+        WebElement todo = browser.findElement(todoElement);
+        String todoClasses = todo.getAttribute("class");
+        assertThat("Todo should be marked as completed", todoClasses, equalTo(completitionMark));
+    }
+
+    private void userChecksIfCompletedTodoIsNOTonActiveList(String completedName) {
+        By activeFilterElement = By.cssSelector(".filters [href=\"#/active\"]");
+        By todoListElement = By.cssSelector(".todo-list");
+        browser.findElement(activeFilterElement).click();
+        WebElement todos = browser.findElement(todoListElement);
+        assertThat("Completed todo is not on active list", todos.getText(), not(containsString(completedName)));
+    }
+
+    private void userChecksIfCompletedTodoIsOnCompletedList(String completedName) {
+        By completedFilterElement = By.cssSelector(".filters [href=\"#/completed\"]");
+        By todoListElement = By.cssSelector(".todo-list");
+        browser.findElement(completedFilterElement).click();
+        WebElement todos = browser.findElement(todoListElement);
+        assertThat("Completed todo is on completed list", todos.getText(), containsString(completedName));
     }
 }
