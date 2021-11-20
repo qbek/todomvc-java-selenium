021bf01ed97272ee3d0849ad2151d63e5793237d
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2021-11-20 12:32:58 +0100

    Tworzenie todo - refaktor

--- src/test/java/org/example/CreateTodoTest.java
+++ src/test/java/org/example/CreateTodoTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,22 +13,39 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CreateTodoTest {
 
+    WebDriver browser = new FirefoxDriver();
+
+    @After
+    public void finishTest() {
+        browser.close();
+    }
 
     @Test
-    public void user_can_create_a_new_todo() {
-        WebDriver browser = new FirefoxDriver();
+    public void user_can_create_a_new_todo() throws InterruptedException {
+        String todoName = "Nauczyć się automatyzacji testów";
+        userOpensTodoMvcApplication();
+        userCreatesANewTodo(todoName);
+        userChecksIfTodoWasCreated(todoName);
+    }
 
-        browser.get("https://todomvc.com/examples/jquery/#/all");
+    private void userOpensTodoMvcApplication() throws InterruptedException {
+        int sleepTime = 1000;
+        String todoMVC_URL = "https://todomvc.com/examples/jquery/#/all";
+        browser.get(todoMVC_URL);
+        Thread.sleep(sleepTime);
+    }
 
-        WebElement input = browser.findElement(By.cssSelector(".new-todo"));
-        input.sendKeys("Nauczyć się automatyzacji testów");
-        input.sendKeys(Keys.RETURN);
+    private void userCreatesANewTodo(String name) {
+        By inputTodo = By.cssSelector(".new-todo");
+        WebElement newTodoInput = browser.findElement(inputTodo);
+        newTodoInput.sendKeys(name);
+        newTodoInput.sendKeys(Keys.RETURN);
+    }
 
-        WebElement list = browser.findElement(By.cssSelector(".todo-list li"));
-        String todo = list.getText();
-
-        MatcherAssert.assertThat("Opis", todo, Matchers.equalTo("Nauczyć się automatyzacji testów"));
-
-        browser.close();
+    private void userChecksIfTodoWasCreated(String expectedName) {
+        By todoList = By.cssSelector(".todo-list li");
+        WebElement todos = browser.findElement(todoList);
+        String todoName = todos.getText();
+        MatcherAssert.assertThat("Todo list shoudl contain created todo", todoName, Matchers.equalTo(expectedName));
     }
 }
