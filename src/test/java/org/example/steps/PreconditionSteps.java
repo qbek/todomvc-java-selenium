18e9312e672fd0641bbd8e63ceef55570c6f942b
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-09-23 10:46:33 +0200

    Raport serenity

--- src/test/java/org/example/steps/PreconditionSteps.java
+++ src/test/java/org/example/steps/PreconditionSteps.java
@@ -1,11 +1,13 @@
 package org.example.steps;
 
+import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
 
 public class PreconditionSteps {
 
     @Steps(shared = true)
     UserSteps steps;
+
 
     public void userHasCompletedTask() {
         steps.userAddsANewTodo();
