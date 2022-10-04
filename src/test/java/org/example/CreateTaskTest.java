a78a302605186e96dae1a52a69c9ee82d2cd274b
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-10-04 11:50:32 +0200

    Second test

--- src/test/java/org/example/CreateTaskTest.java
+++ src/test/java/org/example/CreateTaskTest.java
@@ -24,11 +24,55 @@ public class CreateTaskTest {
         browser.close();
     }
 
+
+
     @Test
-    public void userCanCreateATodo() throws InterruptedException {
+    public void userCanCreateATodo() {
         var todoName = "To jest moje fajniejsze zadanie";
         userCreatesANewTodo(todoName);
         userChecksIfTodoWasCreated(todoName);
+    }
+
+    @Test
+    public void userCanCompleteTodo() {
+        var todoName = "To jest zadanie do zamknięcia";
+        userCreatesANewTodo(todoName);
+        userCompletesTodo();
+        userChecksItTodoIsMarkedAsCompleted();
+        userChecksIfCompletedTodoIsNotOnActiveFilter(todoName);
+        userChecksIfCompletedTodoIsOnCompletedFilter(todoName);
+    }
+
+    private void userChecksIfCompletedTodoIsOnCompletedFilter(String name) {
+        var completedFilter = browser.findElement(By.cssSelector("[href=\"#/completed\""));
+        completedFilter.click();
+
+        var todosList = browser.findElement(By.cssSelector(".todo-list"));
+        var todos = todosList.getText();
+        MatcherAssert.assertThat("Todo is not on the list", todos, Matchers.equalTo(name));
+    }
+
+    private void userChecksIfCompletedTodoIsNotOnActiveFilter(String name) {
+        var activeFilter = browser.findElement(By.cssSelector("[href=\"#/active\""));
+        activeFilter.click();
+
+        var todosList = browser.findElement(By.cssSelector(".todo-list"));
+        var todos = todosList.getText();
+        MatcherAssert.assertThat("Todo is not on the list", todos, Matchers.emptyOrNullString());
+    }
+
+    private void userChecksItTodoIsMarkedAsCompleted() {
+//        var todoElement = browser.findElement(By.cssSelector(".todo-list > li"));
+//        var todoClasses = todoElement.getAttribute("class");
+//        MatcherAssert.assertThat("Todo should be marked as completed", todoClasses, Matchers.equalTo("completed"));
+
+        var todoElements = browser.findElements(By.cssSelector(".todo-list > .completed"));
+        MatcherAssert.assertThat("Todo marked as completed is on the list", todoElements.size(), Matchers.equalTo(1));
+    }
+
+    private void userCompletesTodo() {
+        var todoCompleteToggle = browser.findElement(By.cssSelector(".toggle"));
+        todoCompleteToggle.click();
     }
 
 
