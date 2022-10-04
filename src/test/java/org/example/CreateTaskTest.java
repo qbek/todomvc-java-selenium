65aa306ff538d7b02ce2d9e1bd1bd0ee8e94ad22
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-10-04 10:08:17 +0200

    Refactored create todo test

--- src/test/java/org/example/CreateTaskTest.java
+++ src/test/java/org/example/CreateTaskTest.java
@@ -2,28 +2,49 @@ package org.example;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CreateTaskTest {
 
+    WebDriver browser = new FirefoxDriver();
+
+    @BeforeEach
+    public void setup() {
+        userOpenTodoMVC();
+    }
+
+    @AfterEach
+    public void cleanup() {
+        browser.close();
+    }
+
     @Test
     public void userCanCreateATodo() throws InterruptedException {
-        var browser = new FirefoxDriver();
-        browser.get("https://todomvc.com/examples/jquery/#/all");
-        ////By.xpath("//*contains([@class, 'new-todo']"));
-        var todoInput = browser.findElement(By.cssSelector(".new-todo"));
-        todoInput.sendKeys("To jest moje zadanie");
-        todoInput.sendKeys(Keys.RETURN);
+        var todoName = "To jest moje fajniejsze zadanie";
+        userCreatesANewTodo(todoName);
+        userChecksIfTodoWasCreated(todoName);
+    }
 
+
+    private void userChecksIfTodoWasCreated(String todoName) {
         var todosList = browser.findElement(By.cssSelector(".todo-list"));
         var todos = todosList.getText();
+        MatcherAssert.assertThat("Todo has correct name", todos, Matchers.equalTo(todoName));
+    }
 
-        MatcherAssert.assertThat("Todo has correct name", todos, Matchers.equalTo("To jest moje zadanie"));
+    private void userOpenTodoMVC() {
+        browser.get("https://todomvc.com/examples/jquery/#/all");
+    }
 
-        Thread.sleep(2000);
-        browser.close();
+    private void userCreatesANewTodo(String name) {
+        var todoInput = browser.findElement(By.cssSelector(".new-todo"));
+        todoInput.sendKeys(name);
+        todoInput.sendKeys(Keys.RETURN);
     }
 }
