278e1a29216d4c16d76e7b1704508c18adb99527
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-12-08 10:59:13 +0100

    Refactor algorytmu trawersowania

---
+++ src/test/java/org/example/auxiliary/CustomAssertionErrorFactory.java
@@ -0,0 +1,11 @@
+package org.example.auxiliary;
+
+import java.util.function.Supplier;
+
+public class CustomAssertionErrorFactory {
+
+
+    public static Supplier todoNotFoundError(String name) {
+        return () -> new AssertionError("Todo not exists on page: " + name);
+    }
+}
--- src/test/java/org/example/pageobjects/TodosList.java
+++ src/test/java/org/example/pageobjects/TodosList.java
@@ -1,26 +1,22 @@
 package org.example.pageobjects;
 
 import net.serenitybdd.core.pages.PageObject;
-import net.serenitybdd.core.pages.SerenityActions;
 import net.serenitybdd.core.pages.WebElementFacade;
-import net.serenitybdd.core.pages.WithByLocator;
 import net.thucydides.core.annotations.Step;
-import org.hamcrest.Matchers;
+import org.awaitility.Awaitility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.lang.String.format;
+import static org.example.auxiliary.CustomAssertionErrorFactory.todoNotFoundError;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.openqa.selenium.By.cssSelector;
 
 public class TodosList extends PageObject {
-
     private static final By TODOS_LIST = cssSelector(".todo-list");
     private static final By TODO_ELEMENT = cssSelector(".todo-list li");
     private static final By TODO_COMPLETE_TOGGLE = cssSelector(".toggle");
@@ -76,12 +72,10 @@ public class TodosList extends PageObject {
     }
 
     private WebElementFacade findTodoWithName(String name) {
-        var allTodos = findAll(TODO_ELEMENT);
-        for (var todo : allTodos) {
-            if (todo.getText().equals(name)) {
-                return todo;
-            }
+           return findEach(TODO_ELEMENT)
+                    .filter(todo -> (todo.getText().equals(name)))
+                    .findFirst().orElseThrow(todoNotFoundError(name));
+    }
         }
-        throw new AssertionError(format("Todo with %s name dosn't exist", name));
     }
 }
