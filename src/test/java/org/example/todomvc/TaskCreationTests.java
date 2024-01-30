72a50d6827ebe0b99fa6fcdb49b8bbcf11fa5c2b
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2024-01-30 11:25:26 +0100

    Refaktor dodania zadania

--- src/test/java/org/example/todomvc/TaskCreationTests.java
+++ src/test/java/org/example/todomvc/TaskCreationTests.java
@@ -2,6 +2,7 @@ package org.example.todomvc;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -11,21 +12,44 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TaskCreationTests {
 
-    @Test
-    public void userCanCreateANewTask () {
-        WebDriver browser = new FirefoxDriver();
-        browser.get("https://todomvc.com/examples/angular/dist/browser/#/all");
+    By newTodoInputEl = By.cssSelector(".new-todo");
+    By todoListEl = By.cssSelector(".todo-list");
 
-        WebElement newTodoInput = browser.findElement(By.cssSelector(".new-todo"));
-        newTodoInput.sendKeys("Moje zadanie");
-        newTodoInput.sendKeys(Keys.ENTER);
-
-        WebElement todoItem = browser.findElement(By.cssSelector(".todo-list label"));
-        MatcherAssert.assertThat("Todo has correct name", todoItem.getText(), Matchers.equalTo("Moje zadanie"));
+    WebDriver browser = new FirefoxDriver();
 
 
+    @AfterEach
+    public void cleanup() {
         browser.close();
     }
 
+    @Test
+    public void userCanCreateANewTask () {
+        String todoName = "Moje zadanie";
+        userOpensTodoMVCapp();
+        userCreatesANewTask(todoName);
+        userChecksIfTaskCreated(todoName);
+    }
+
+    @Test
+    public void userCanCompleteTask () {
+        
+    }
+
+    private void userChecksIfTaskCreated(String todoName) {
+        //User checks if todo is created
+        WebElement todoItem = browser.findElement(todoListEl);
+        MatcherAssert.assertThat("Todo has correct name", todoItem.getText(), Matchers.equalTo(todoName));
+    }
+
+    private void userCreatesANewTask(String todoName) {
+        WebElement newTodoInput = browser.findElement(newTodoInputEl);
+        newTodoInput.sendKeys(todoName);
+        newTodoInput.sendKeys(Keys.ENTER);
+    }
+
+    private void userOpensTodoMVCapp() {
+        browser.get("https://todomvc.com/examples/angular/dist/browser/#/all");
+    }
 
 }
