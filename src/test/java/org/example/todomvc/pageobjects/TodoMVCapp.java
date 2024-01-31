10f6012fa52eab79dc5722c9036b3b2cee90e99a
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2024-01-31 14:21:58 +0100

    Zmienne srodowiskowe

--- src/test/java/org/example/todomvc/pageobjects/TodoMVCapp.java
+++ src/test/java/org/example/todomvc/pageobjects/TodoMVCapp.java
@@ -3,10 +3,22 @@ package org.example.todomvc.pageobjects;
 import net.serenitybdd.annotations.Step;
 import net.serenitybdd.core.pages.PageObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class TodoMVCapp extends PageObject {
 
     @Step
     public void openTodoMVC () {
-        getDriver().get("https://todomvc.com/examples/angular/dist/browser/#/all");
+        try {
+            String envName = System.getProperty("env");
+            InputStream file = this.getClass().getResourceAsStream("/" + envName + ".env.properties");
+            Properties cfg = new Properties();
+            cfg.load(file);
+            getDriver().get(cfg.getProperty("todoMVC_url"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
