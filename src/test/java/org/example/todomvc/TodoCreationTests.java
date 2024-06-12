1b698f58bec08a36ebb7e5c9288e77eabd842142
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2024-06-12 11:41:52 +0200

    Refactor of User can create todo test

--- src/test/java/org/example/todomvc/TodoCreationTests.java
+++ src/test/java/org/example/todomvc/TodoCreationTests.java
@@ -2,9 +2,12 @@ package org.example.todomvc;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -14,18 +17,43 @@ import static org.openqa.selenium.Keys.*;
 
 public class TodoCreationTests {
 
+    private By newTodoInputEl = cssSelector(".new-todo");
+    private By todoListEl = cssSelector(".todo-list");
+
+    private String todoMvcURL = "https://todomvc.com/examples/angular/dist/browser/#/all";
+
+    private WebDriver browser = new FirefoxDriver();
+
+    @AfterEach
+    public void cleanup() {
+        userClosesTodoMVCapp();
+    }
+
     @Test
     public void userCanCreateATodo() {
-        var browser = new FirefoxDriver();
-        browser.get("https://todomvc.com/examples/angular/dist/browser/#/all");
+        var todoName = "Moje lepsze zadanie 2";
 
-        var newTodoInput = browser.findElement(cssSelector(".new-todo"));
-        newTodoInput.sendKeys("Moje zadanie");
-        newTodoInput.sendKeys(ENTER);
+        userOpensTodoMVCapp();
+        userCreatesANewTodo(todoName);
+        userChecksIfTodoIsCreated(todoName);
+    }
 
-        var todoItem = browser.findElement(cssSelector(".todo-list"));
-        assertThat("Todo has correct name", todoItem.getText(), equalTo("Moje zadanie"));
+    private void userOpensTodoMVCapp() {
+        browser.get(todoMvcURL);
+    }
 
+    private void userClosesTodoMVCapp() {
         browser.close();
+    }
+
+    private void userCreatesANewTodo(String name) {
+        var newTodoInput = browser.findElement(newTodoInputEl);
+        newTodoInput.sendKeys(name);
+        newTodoInput.sendKeys(ENTER);
+    }
+
+    private void userChecksIfTodoIsCreated(String expectedName) {
+        var todoItem = browser.findElement(todoListEl);
+        assertThat("Todo has correct name", todoItem.getText(), equalTo(expectedName));
     }
 }
