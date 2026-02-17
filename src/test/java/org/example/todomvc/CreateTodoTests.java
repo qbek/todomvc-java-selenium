fe153f426d4bdae4d68f6ceb2d78ed8d4fbb7982
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2026-02-17 11:56:44 +0100

    Test: user can complete todo

--- src/test/java/org/example/todomvc/CreateTodoTests.java
+++ src/test/java/org/example/todomvc/CreateTodoTests.java
@@ -12,6 +12,14 @@ public class CreateTodoTests {
 
     private By newTodoInputSelector =  By.cssSelector("#new-todo");
     private By todoListSelector = By.cssSelector("#todo-list");
+    private By todoItemSelector = By.cssSelector("#todo-list li");
+    private By todoCompleteChkboxSelector = By.cssSelector(".toggle");
+
+    private By activeTabSelector = By.cssSelector("[href=\"#/active\"]");
+    private By completedTabSelector = By.cssSelector("[href=\"#/completed\"]");
+
+    private By xpathCost = By.xpath("asdfasdf");
+
     private String TodoMVC_URL = "https://todomvc.com/examples/jquery/dist/#/all";
 
 
@@ -36,6 +44,49 @@ public class CreateTodoTests {
                 todoList.getText(),
                 Matchers.equalTo(todoName));
 
+        browser.close();
+    }
+
+    @Test
+    public void userCanCompleteTheTodo() {
+        var todoName = "Zadanie do zakończenia";
+        var browser = new FirefoxDriver();
+        browser.get(TodoMVC_URL);
+
+        var newTodoInput = browser.findElement(newTodoInputSelector);
+        newTodoInput.sendKeys(todoName);
+        newTodoInput.sendKeys(Keys.RETURN);
+
+        var todoCompleteBtn = browser.findElement(todoCompleteChkboxSelector);
+        todoCompleteBtn.click();
+
+        var todoItem = browser.findElement(todoItemSelector);
+        MatcherAssert.assertThat("Todo is marked as completed",
+                todoItem.getAttribute("class"),
+                Matchers.containsString("completed"));
+
+        var activeTab = browser.findElement(activeTabSelector);
+        activeTab.click();
+
+//        sprawdzanie czy lista jest pusta przy założeniu że lista jest na stronie
+//        var todoList = browser.findElement(todoListSelector); // nie dopuszcza braku elementu -> rzuca wyjatek
+//        MatcherAssert.assertThat("Todo has correct name",
+//                todoList.getText(),
+//                Matchers.emptyOrNullString());
+
+
+//        sprawdzenie czy NIE MA takiego elementu
+        var todoItems = browser.findElements(todoItemSelector); //dopuszcza brak elementu -> zwraca pusta liste
+        MatcherAssert.assertThat("Empty todo list",
+                todoItems,
+                Matchers.empty());
+
+        var todoList = browser.findElement(todoListSelector);
+        var completedTab = browser.findElement(completedTabSelector);
+        completedTab.click();
+        MatcherAssert.assertThat("Todo has correct name",
+                todoList.getText(),
+                Matchers.equalTo(todoName));
         browser.close();
 
     }
