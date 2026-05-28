3d9776600a90574331e3448d3322ddbaecb2a6e1
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2026-05-28 10:18:08 +0200

    New test: User can complete todo

--- src/test/java/org/example/todomvc/CreateTodoTest.java
+++ src/test/java/org/example/todomvc/CreateTodoTest.java
@@ -16,7 +16,7 @@ public class CreateTodoTest {
     private final static String TODO_URL = "https://todomvc.com/examples/jquery/dist/#/all";
 
     private final static By NEW_TODO_INPUT_SELECTOR =  By.cssSelector("#new-todo");
-    private final static By TODO_LABEL_SELECTOR = By.cssSelector("#todo-list label");
+    private final static By TODO_LIST = By.cssSelector("#todo-list");
 
     private WebDriver browser = new FirefoxDriver();
 
@@ -35,9 +35,45 @@ public class CreateTodoTest {
         newTodoInput.sendKeys(todoName);
         newTodoInput.sendKeys(Keys.ENTER);
 
-        var todoLabel = browser.findElement(TODO_LABEL_SELECTOR);
+        var todoLabel = browser.findElement(TODO_LIST);
         MatcherAssert.assertThat("Created todo has valid name",
                 todoLabel.getText(),
-                Matchers.equalTo(todoName + "asdfads"));
+                Matchers.equalTo(todoName));
+    }
+
+    @Test
+    public void userCanCompleteATodo() {
+        var todoName = "Zadanie do zakończenia";
+        browser.get(TODO_URL);
+
+        var newTodoInput = browser.findElement(NEW_TODO_INPUT_SELECTOR);
+        newTodoInput.sendKeys(todoName);
+        newTodoInput.sendKeys(Keys.ENTER);
+
+        var todoCompleteToggle = browser.findElement(By.cssSelector(".toggle"));
+        todoCompleteToggle.click();
+
+        var todoElement = browser.findElement(By.cssSelector("#todo-list li"));
+        MatcherAssert.assertThat("Todo is marked as completed",
+                todoElement.getAttribute("class"),
+                Matchers.containsString("completed")
+        );
+
+        var activeTodosFilter = browser.findElement(By.cssSelector("[href=\"#/active\"]"));
+        activeTodosFilter.click();
+
+        var todosList = browser.findElement(TODO_LIST);
+        MatcherAssert.assertThat("Todos list is emtpy",
+                todosList.getText(),
+                Matchers.emptyString());
+
+        var completedTodosFilter = browser.findElement(By.cssSelector("[href=\"#/completed\"]"));
+        completedTodosFilter.click();
+
+        var todoLabel = browser.findElement(TODO_LIST);
+        MatcherAssert.assertThat("Todo is marked as completed",
+                todoLabel.getText(),
+                Matchers.equalTo(todoName)
+        );
     }
 }
